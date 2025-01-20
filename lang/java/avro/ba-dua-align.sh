0fd25c4cb5a6b1254d0e11a0b200494bedd7b621
AlessandroFinocchi <alessandro.22082001@gmail.com>
2025-01-20 22:43:15 +0100

    setup project

--- lang/java/avro/ba-dua-align.sh
+++ lang/java/avro/ba-dua-align.sh
@@ -1,2 +1,12 @@
-xmllint --format target/badua.xml > target/badua_pretty.xml
-rm target/badua.xml
+input_file="badua.xml"
+modified_file="badua_pretty.xml"
+schema_class="org/apache/avro/Schema"
+
+xmllint --format target/$input_file > target/$modified_file
+
+rm target/$input_file
+cp "target/$modified_file" "target/badua_Schema.xml"
+
+xmlstarlet ed -L -d \
+ "//class[not(contains(@name, '$schema_class'))]" \
+ "target/badua_Schema.xml"
