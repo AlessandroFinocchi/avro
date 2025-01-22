be30c0c63cad26f2c51423ce4c17d0cd41fbd1f0
AlessandroFinocchi <alessandro.22082001@gmail.com>
2025-01-22 12:29:50 +0100

    final commit for setup tools

--- lang/java/avro/src/test/java/org/apache/avro/SchemaTest.java
+++ lang/java/avro/src/test/java/org/apache/avro/SchemaTest.java
@@ -1,40 +1,35 @@
 package org.apache.avro;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.Timeout;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.Test;
 
-import java.util.stream.Stream;
-
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SchemaTest {
-
-  private static Stream<Arguments> data() {
-    return Stream.of(
-        // Varying allocator
-        Arguments.of(Schema.Type.INT, null),
-        Arguments.of(Schema.Type.LONG, null)
-    );
+  public static Object[][] testData() {
+    return new Object[][] {
+        { Schema.Type.INT,  null },
+        { Schema.Type.LONG, null }
+    };
   }
 
-  @ParameterizedTest
-  @MethodSource("data")
-  @Timeout(value = 5, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
-  public void testSchema(Schema.Type t, Class<Exception> expectedException) {
-    if (expectedException != null) Assert.assertTrue(true);
+  public SchemaTest() {}
+
+  @Test public void testIntSchema() { testSchema(0); }
+  @Test public void testLongSchema() { testSchema(1); }
+
+  private void testSchema(int index) {
+    Schema.Type schemaType = (Schema.Type) testData()[index][0];
+    Class<Exception> expectedException = (Class<Exception>) testData()[index][1];
+
+    if (expectedException != null) Assert.assertThrows(expectedException, () -> Schema.create(schemaType));
     else{
       try {
-        Schema schema1 = Schema.create(t);
-        Schema schema2 = Schema.create(t);
+        Schema actualSchema = Schema.create(schemaType);
+        Schema expectedSchema = Schema.create(schemaType);
 
-        Assert.assertEquals(schema1, schema2);
+        Assert.assertEquals(expectedSchema, actualSchema);
       } catch (Exception e) {
         throw new RuntimeException(e);
       }
     }
   }
 }
-
