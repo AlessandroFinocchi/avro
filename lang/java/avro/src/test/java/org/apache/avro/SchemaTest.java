73db0f0c44e5d4b146ff61030a4a5d3e2d784f6e
AlessandroFinocchi <alessandro.22082001@gmail.com>
2025-01-22 10:31:19 +0100

    setup project 5

---
+++ lang/java/avro/src/test/java/org/apache/avro/SchemaTest.java
@@ -0,0 +1,39 @@
+package org.apache.avro;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SchemaTest {
+
+  private static Stream<Arguments> data() {
+    return Stream.of(
+        // Varying allocator
+        Arguments.of(Schema.Type.INT, null)
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("data")
+  @Timeout(value = 5, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
+  public void testSchema(Schema.Type t, Class<Exception> expectedException) {
+    if (expectedException != null) Assert.assertTrue(true);
+    else{
+      try {
+        Schema schema1 = Schema.create(t);
+        Schema schema2 = Schema.create(t);
+
+        Assert.assertEquals(schema1, schema2);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+}
+
