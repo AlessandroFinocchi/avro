1cee80d5d3078dd296958971a4c31af4cba133e5
AlessandroFinocchi <alessandro.22082001@gmail.com>
2025-01-30 19:35:15 +0100

    Compatibility: update config

--- lang/java/avro/ba-dua-align.sh
+++ lang/java/avro/ba-dua-align.sh
@@ -1,12 +1,18 @@
 input_file="badua.xml"
 modified_file="badua_pretty.xml"
 schema_class="org/apache/avro/Schema"
+compat_class="org/apache/avro/SchemaCompatibility"
 
 xmllint --format target/$input_file > target/$modified_file
 
 rm target/$input_file
 cp "target/$modified_file" "target/badua_Schema.xml"
+cp "target/$modified_file" "target/badua_SchemaCompatibility.xml"
 
 xmlstarlet ed -L -d \
  "//class[not(contains(@name, '$schema_class'))]" \
+ "target/badua_Schema.xml"
+
+xmlstarlet ed -L -d \
+ "//class[not(contains(@name, '$compat_class'))]" \
  "target/badua_Schema.xml"
